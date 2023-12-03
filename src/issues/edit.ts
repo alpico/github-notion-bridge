7eb39040c0a4b8d5d7f4ff5cb69ee9bd1d4f34c6
Ari Breitkreuz <ari.breitkreuz@pm.me>
2023-12-03 17:10:39 +0100

    implement edit

--- src/issues/edit.ts
+++ src/issues/edit.ts
@@ -1,11 +1,52 @@
 import { Context } from "@actions/github/lib/context";
 import { Client } from "@notionhq/client"
+import { BlockObjectRequest } from "@notionhq/client/build/src/api-endpoints";
 import { apiKey, pageId } from "../main";
 import { githubLinkFromIssue, notionPageIdsFromGithubLink } from ".";
+import { markdownToBlocks } from "@tryfabric/martian";
 
 export async function edit(context: Context): Promise<void> {
     const notion = new Client({ auth: apiKey });
     const link = githubLinkFromIssue(context);
     const issuePageIds = await notionPageIdsFromGithubLink(notion, pageId, link);
-    // TODO
+
+    issuePageIds.forEach(async issuePageId => {
+        if (context.payload.changes.title?.from) {
+            await updateTitle(notion, issuePageId, context.payload.issue?.["title"] ?? "");
+        }
+        if (context.payload.changes.body?.from) {
+            await updateBody(notion, issuePageId, context.payload.issue?.body ?? "")
+        }
+    })
+}
+
+async function updateTitle(notion: Client, pageId: string, new_title: string): Promise<void> {
+    const response = await notion.pages.update({
+        page_id: pageId,
+        properties: {
+            "title": {
+                title: [{ text: { content: new_title } }]
+            }
+        },
+    })
+    console.log(response);
+}
+
+async function updateBody(notion: Client, pageId: string, newBody: string): Promise<void> {
+    await deleteChildBlocks(notion, pageId);
+    const response = await notion.blocks.children.append({
+        block_id: pageId,
+        children: markdownToBlocks(newBody) as Array<BlockObjectRequest>,
+    })
+    console.log(response);
+}
+
+async function deleteChildBlocks(notion: Client, pageId: string): Promise<void> {
+    // Get child blocks
+    const childBlocks = await notion.blocks.children.list({ block_id: pageId });
+    // Remove all child blocks. Use for loop to ensure synchronicity because
+    // otherwise we get 409s.
+    for (const block of childBlocks.results) {
+        await notion.blocks.delete({ block_id: block.id });
+    };
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import * as core from '@actions/core';
-import contextObject from './sample_events/deleted.json';
+import contextObject from './sample_events/edit_title.json';
 import { Context } from '@actions/github/lib/context';
 import * as issues from './issues';
 import { config } from "dotenv";
@@ -51,18 +51,45 @@ export async function run(): Promise<void> {
 
 async function handleIssuesEvent(context: Context): Promise<void> {
   switch (context.payload.action as IssuesAction) {
-    case "opened": await issues.open(context)
-    case "edited": await issues.edit(context)
-    case "deleted": await issues.deletePage(context)
-    case "transferred": throw new Error("`transferred` events are not yet implemented")
-    case "pinned": throw new Error("`pinned` events are not yet implemented")
-    case "unpinned": throw new Error("`unpinned` events are not yet implemented")
-    case "closed": await issues.close(context)
-    case "reopened": await issues.reopen(context)
-    case "assigned": await issues.assign(context)
-    case "unassigned": await issues.unassign(context)
-    case "labeled": await issues.label(context)
-    case "unlabeled": await issues.unlabel(context)
+    case "opened": {
+      await issues.open(context);
+      break;
+    }
+    case "edited": {
+      await issues.edit(context)
+      break;
+    }
+    case "deleted": {
+      await issues.deletePage(context);
+      break;
+    }
+    case "transferred": throw new Error("`transferred` events are not yet implemented");
+    case "pinned": throw new Error("`pinned` events are not yet implemented");
+    case "unpinned": throw new Error("`unpinned` events are not yet implemented");
+    case "closed": {
+      await issues.close(context);
+      break;
+    }
+    case "reopened": {
+      await issues.reopen(context);
+      break;
+    }
+    case "assigned": {
+      await issues.assign(context);
+      break;
+    }
+    case "unassigned": {
+      await issues.unassign(context);
+      break;
+    }
+    case "labeled": {
+      await issues.label(context);
+      break;
+    }
+    case "unlabeled": {
+      await issues.unlabel(context);
+      break;
+    }
     case "locked": throw new Error("`locked` events are not yet implemented")
     case "unlocked": throw new Error("`unlocked` events are not yet implemented")
     case "milestoned": throw new Error("`milestoned` events are not yet implemented")
