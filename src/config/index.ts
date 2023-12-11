755751de1957561d6a091e235f07656cd3c1a2ca
Ari Breitkreuz <ari@alpico.io>
2023-12-11 16:24:55 +0100

    Add more debug

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,6 @@
 import { config as config_local } from "./config_local";
 import { config as config_action } from "./config_action"
+import * as core from "@actions/core";
 
 export type Config = {
     ghToken: string,
@@ -19,7 +20,9 @@ export type Config = {
 
 export let config: Config;
 if (process.argv.find(x => x === "--local") !== null) {
+    core.info("Using local configuration");
     config = config_local();
 } else {
+    core.info("Using action configuration...");
     config = config_action();
 }
